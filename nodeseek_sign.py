323fd18c18cbee13e565e3d4463a0cc77c40f64a
yanlc <yanleichang@vip.163.com>
2024-11-29 10:44:56 +0800

    debug

--- nodeseek_sign.py
+++ nodeseek_sign.py
@@ -3,16 +3,18 @@ import os
 import sys
 from curl_cffi import requests
 
-NS_RANDOM = os.environ.get("NS_RANDOM","true")
-NS_COOKIE = os.environ.get("NS_COOKIE","")
+NS_RANDOM = os.environ.get("NS_RANDOM", "true")
+NS_COOKIE = os.environ.get("NS_COOKIE", "")
 COOKIE = os.environ.get("COOKIE", "")
 COOKIE_ENV = NS_COOKIE or COOKIE
 
 pushplus_token = os.environ.get("PUSHPLUS_TOKEN")
-telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN","")
-chat_id = os.environ.get("CHAT_ID","")
-telegram_api_url = os.environ.get("TELEGRAM_API_URL","https://api.telegram.org") # 代理api,可以使用自己的反代
-def telegram_Bot(token,chat_id,message):
+telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+chat_id = os.environ.get("CHAT_ID", "")
+telegram_api_url = os.environ.get("TELEGRAM_API_URL", "https://api.telegram.org")  # 代理api,可以使用自己的反代
+
+
+def telegram_Bot(token, chat_id, message):
     url = f'{telegram_api_url}/bot{token}/sendMessage'
     data = {
         'chat_id': chat_id,
@@ -22,6 +24,8 @@ def telegram_Bot(token,chat_id,message):
     response_data = r.json()
     msg = response_data['ok']
     print(f"telegram推送结果：{msg}\n")
+
+
 def pushplus_ts(token, rw, msg):
     url = 'https://www.pushplus.plus/send/'
     data = {
@@ -33,6 +37,7 @@ def pushplus_ts(token, rw, msg):
     msg = r.json().get('msg', None)
     print(f'pushplus推送结果：{msg}\n')
 
+
 def load_send():
     global send
     global hadsend
@@ -41,33 +46,41 @@ def load_send():
     if os.path.exists(cur_path + "/notify.py"):
         try:
             from notify import send
-            hadsend=True
+            hadsend = True
         except:
             print("加载notify.py的通知服务失败，请检查~")
-            hadsend=False
+            hadsend = False
     else:
         print("加载通知服务失败,缺少notify.py文件")
-        hadsend=False
+        hadsend = False
+
+
 load_send()
 
 if COOKIE_ENV:
     url = f"https://www.nodeseek.com/api/attendance?random={NS_RANDOM}"
     headers = {
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
-        'sec-ch-ua': "\"Not A(Brand\";v=\"99\", \"Microsoft Edge\";v=\"121\", \"Chromium\";v=\"121\"",
-        'sec-ch-ua-mobile': "?0",
-        'sec-ch-ua-platform': "\"Windows\"",
-        'origin': "https://www.nodeseek.com",
-        'sec-fetch-site': "same-origin",
-        'sec-fetch-mode': "cors",
-        'sec-fetch-dest': "empty",
-        'referer': "https://www.nodeseek.com/board",
-        'accept-language': "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        'accept': '*/*',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'origin': 'https://www.nodeseek.com',
+        'priority': 'u=1, i',
+        'referer': 'https://www.nodeseek.com/board',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
         'Cookie': COOKIE_ENV
     }
 
+    params = {
+        'random': NS_RANDOM
+    }
+
     try:
-        response = requests.post(url, headers=headers,impersonate="chrome110")
+        response = requests.post(url, headers=headers, impersonate="chrome110")
         response_data = response.json()
         print(response_data)
         print(COOKIE_ENV)
